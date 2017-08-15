a8b2902bc0ff608f6071a23e87fac9131f6c3c2a
Robby Bourne <robbybourne1@gmail.com>
2017-08-15 10:09:42 -0400

    Ran Favorite Baseball Team

--- Program.cs
+++ Program.cs
@@ -4,9 +4,34 @@ namespace lifeModel_dotnet
 {
     class Program
     {
+        public class SportsTeam {
+            public string Sport { get; set; }
+            public string Colors { get; set; }
+            public int Championships { get; set; } = 0;
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return $"My favorite {Sport} team is {Name} and their colors are {Colors} and they have won {Championships} championships.";
+            }
+
+            // public SportsTeam(string sport, string colors, string name )
+            // {
+            //     Console.WriteLine("Adding New Team");
+            //     Sport = sport;
+            //     Colors = colors;
+            //     Name = name; 
+            // }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var team = new SportsTeam();
+
+            team.Name = "St. Louis Cardinals";
+            team.Sport = "Baseball";
+            team.Colors = "Red and White";
+            team.Championships = 11;
+            Console.WriteLine(team);
         }
     }
 }
